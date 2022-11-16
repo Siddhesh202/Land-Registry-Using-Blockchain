73744c1b720a9f5a422fb80507a256c5f6108d61
Siddhesh202 <siddheshpisal76@gmail.com>
2022-11-16 17:42:37 +0530

    Final2 Commit

--- lib/constant/constants.dart
+++ lib/constant/constants.dart
@@ -2,7 +2,7 @@ String mapBoxApiKey =
     'pk.eyJ1Ijoic2lkMzAwMSIsImEiOiJjbGFqOGtseTEwYWxsM3FuM3Fubzk4enI0In0.oIJDRwZlwVNKnaa6g-qZcQ';
 
 String nftStorageApiKey =
-    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.5ReEuIxsDhWxOLa2lVe9n-B2PUjdEkwJ5jLsBGdBDGA';
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDhFMjc2ZjM4ZTBkNmE4ZDlhYWQ2OUJBY2RhRjk3YTRBNkFiRkUxQjIiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTY2ODU5OTQ3OTk4MCwibmFtZSI6IkxhbmQgUmVnaXN0cnkgIn0.FHD_IfrChyCXmRZO1xzSrScfI-UMmFGZ2KC6C1kMVzc';
 
 const String rpcUrl = "http://127.0.0.1:7545";
 //"http://127.0.0.1:7545";
