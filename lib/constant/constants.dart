cc68215aceaf2153132f382e1760752de2cc9ad5
Siddhesh202 <siddheshpisal76@gmail.com>
2022-11-16 13:01:24 +0530

    Final Commit

---
+++ lib/constant/constants.dart
@@ -0,0 +1,12 @@
+String mapBoxApiKey =
+    'pk.eyJ1Ijoic2lkMzAwMSIsImEiOiJjbGFqOGtseTEwYWxsM3FuM3Fubzk4enI0In0.oIJDRwZlwVNKnaa6g-qZcQ';
+
+String nftStorageApiKey =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJkaWQ6ZXRocjoweDJmNGUwQTQwNTI4MkMyMDNkZDBEZmY2NUNlMkUwRTYyQUNCODFDRWUiLCJpc3MiOiJuZnQtc3RvcmFnZSIsImlhdCI6MTYzNzkwNzQxNjEwNSwibmFtZSI6ImxhbmRfZG9jdW1lbnQifQ.5ReEuIxsDhWxOLa2lVe9n-B2PUjdEkwJ5jLsBGdBDGA';
+
+const String rpcUrl = "http://127.0.0.1:7545";
+//"http://127.0.0.1:7545";
+
+const String contractAddress = "0xf26366FBb221B28a18dff1A2d1A6eBC528f7B578";
+
+const int chainId = 1337; //Polygon testnet-80001, local ganache-1337
